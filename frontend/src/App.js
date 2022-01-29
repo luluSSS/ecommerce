3fbac97054536055e71bd286a1550932874274bc
Lulu Sun <sunyylulu@gmail.com>
2022-01-28 18:59:26 -0600

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Ecommerce TBD</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
